d7f4b24d93762a91b9d76240c06a9d677738f060
wojdyr <wojdyr@fc17cd6a-f944-0410-b616-addcbd18eba7>
2010-09-10 21:55:38 +0000

    added empty __init__.py

---
+++ __init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+# this file is part of gosam (generator of simple atomistic models)
+# Licence: GNU General Public License version 2
+
+# This files marks the containing directory as Python package.
